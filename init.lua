93f635b2a8b1ea52e754339e5e9c5756a4d5aff2
Thomas Brosch <thomas.brosch@inverso.de>
2026-08-25 21:02:59 +0200

    Change mapping for mini.ai next and last text object to allow usage of argument text objects.

--- init.lua
+++ init.lua
@@ -427,8 +427,8 @@ do
   require('mini.ai').setup {
     -- NOTE: Avoid conflicts with the built-in incremental selection mappings on Neovim>=0.12 (see `:help treesitter-incremental-selection`)
     mappings = {
-      around_next = 'aa',
-      inside_next = 'ii',
+      around_next = 'ax',
+      inside_next = 'ix',
     },
     n_lines = 500,
   }
