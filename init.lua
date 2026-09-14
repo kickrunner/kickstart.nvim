995899e91d31fc05a09a446fe4074981a5ea0d13
Thomas Brosch <thomas.brosch@inverso.de>
2026-09-14 18:55:47 +0200

    Plugin none-ls entfernt, da in kickstart.nvim schon conform-Plugin drin ist, sodass man wunderbar diverse Formatter für z.B. Java u. Javascript konfigurieren kann.

--- init.lua
+++ init.lua
@@ -876,6 +876,8 @@ do
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascript = { 'prettier' },
+      java = { 'spotless_maven' },
     },
   }
 
